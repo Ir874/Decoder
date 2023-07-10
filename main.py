8d96ace770543debfd767f27dee6b9c2cd97e085
Ishaan Rao <ishaan_rao@icloud.com>
2023-07-10 19:09:53 +0530

    started making dictionary

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ from time import sleep
 
 # Function to encode the input
 def encode(x):
-    pass
+    replacedict = {
+        "a" : "p",
+        "b" : "o",
+        "c" : "n",
+        "d" : "m",
+        "e" : "f",
+    }
+
+    
+
 
 # Function to decode the message and print
 def decode(x):
     pass
 # Function to take input
-userin = input("Enter text: ")
 SelectType = input("Encode or Decode: ")
+userin = input("Enter text: ")
 
 # Decides whether encode or decode
 if SelectType == "Encode" or "encode":
-    encode(userin)
+    encode(list(userin))
 elif SelectType == "Decode" or "decode":
     decode(userin)
